017969c97c08bfee25a8f036e57760a8081e57f8
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-04-04 19:24:54 +0200

    wXTM extended with burn fn

--- contracts/wXTM.sol
+++ contracts/wXTM.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {OFT} from "@layerzerolabs/oft-evm/contracts/OFT.sol";
 
-contract wXTM is OFT  {
+contract wXTM is OFT {
     constructor(
         string memory _name,
         string memory _symbol,
@@ -12,8 +12,12 @@ contract wXTM is OFT  {
         address _delegate // multi-sig
     ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {}
 
-    /** @dev This function is allowed to use by multi-sig-wallet only */
-    function mint(address _to, uint256 _amount) public onlyOwner {
+    /** @dev Below functions are allowed to use by multi-sig-wallet only */
+    function mint(address _to, uint256 _amount) external onlyOwner {
         _mint(_to, _amount);
+    }
+
+    function burn(address _from, uint256 _amount) external onlyOwner {
+        _burn(_from, _amount);
     }
 }
