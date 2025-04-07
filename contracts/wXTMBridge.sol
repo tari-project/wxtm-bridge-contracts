f504aa1d58b41e624e6a3bad0783b8a473b98da2
Nlferu <wargacki.patryq@gmail.com>
2025-04-07 17:26:29 +0200

    wXTM extended with update wXTM address function to handle wXTM upgradeable concept

--- contracts/wXTMBridge.sol
+++ contracts/wXTMBridge.sol
@@ -1,19 +1,25 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import {EIP3009} from "./extensions/EIP3009.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { EIP3009 } from "./extensions/EIP3009.sol";
+import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-import {IwXTM} from "./interfaces/IwXTM.sol";
+import { IwXTM } from "./interfaces/IwXTM.sol";
 
-contract wXTMBridge is EIP3009 {
-    address private immutable wXTM;
+contract wXTMBridge is EIP3009, Ownable {
+    address private wXTM;
 
     event TokensMinted(address indexed to, uint256 indexed amount, bytes32 indexed authorizationId);
     event TokensBurned(address indexed from, uint256 indexed amount, bytes32 indexed authorizationId);
 
-    constructor(address _wXTM, string memory version) EIP3009("wXTMBridge", version) {
+    constructor(address _wXTM) Ownable(msg.sender) {
         wXTM = _wXTM;
+    }
+
+    /** @dev Function to handle wXTM upgradable concept */
+    function updatewXTMAddress(address wXTMAddress) external onlyOwner {
+        wXTM = wXTMAddress;
     }
 
     function _mintTokens(address to, uint256 amount) internal override {
