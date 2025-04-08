213b7bb661d1925b6f8080904bdbd11a8213a0e8
Nlferu <wargacki.patryq@gmail.com>
2025-04-08 18:03:05 +0200

    wXTM refactored to OFTUpgradeable, wXTM interface now includes receiveWithAuthorization fn

--- contracts/interfaces/IwXTM.sol
+++ contracts/interfaces/IwXTM.sol
@@ -5,4 +5,16 @@ interface IwXTM {
     function mint(address _to, uint256 _amount) external;
 
     function burn(address _from, uint256 _amount) external;
+
+    function receiveWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
 }
--- contracts/wXTM.sol
+++ contracts/wXTM.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+import { OFTUpgradeable } from "@layerzerolabs/oft-evm-upgradeable/contracts/oft/OFTUpgradeable.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { EIP3009 } from "./extensions/EIP3009.sol";
 
-contract wXTM is OFT {
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        address _lzEndpoint,
-        address _delegate // multi-sig
-    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {}
+contract wXTM is OFTUpgradeable, EIP3009 {
+    constructor(address _lzEndpoint) OFTUpgradeable(_lzEndpoint) {}
+
+    function initialize(string memory _name, string memory _symbol, address _delegate) external initializer {
+        __OFT_init(_name, _symbol, _delegate);
+        __Ownable_init(_delegate);
+    }
 
     /** @dev Below functions are allowed to use by multi-sig-wallet only */
     function mint(address _to, uint256 _amount) external onlyOwner {
@@ -19,5 +20,19 @@ contract wXTM is OFT {
 
     function burn(address _from, uint256 _amount) external onlyOwner {
         _burn(_from, _amount);
+    }
+
+    function receiveWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        _receiveWithAuthorization(from, to, value, validAfter, validBefore, nonce, v, r, s);
     }
 }
