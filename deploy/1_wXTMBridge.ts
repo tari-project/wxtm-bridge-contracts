50a8858a0c2e2e3b6b727af7db6d6d6f7667ba6b
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-04-10 15:29:47 +0200

    deploy scripts renamed to properly index exec order, wXTMBridge deploy script implemented

---
+++ deploy/1_wXTMBridge.ts
@@ -0,0 +1,46 @@
+import assert from 'assert'
+import verify from '../utils/verify'
+
+import { type DeployFunction } from 'hardhat-deploy/types'
+import { ethers } from 'hardhat'
+import { getDeployments } from '../wxtm-bridge-contracts-typechain/deployments/'
+
+const contractName = 'wXTMBridge'
+
+const deploy: DeployFunction = async (hre) => {
+    const { getNamedAccounts, deployments } = hre
+
+    if (hre.network.name !== 'sepolia-testnet') {
+        console.log(`Deployment script can only run on the Sepolia network. Current network: ${hre.network.name}`)
+        return
+    }
+
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+    const deployedContracts = getDeployments(11155111)
+
+    assert(deployer, 'Missing named deployer account')
+
+    console.log(`wXTM: ${deployedContracts.wXTM}`)
+    console.log(`Network: ${hre.network.name}`)
+    console.log(`Deployer: ${deployer}`)
+
+    const salt = ethers.utils.id('wXTM-deployment')
+
+    const wXTMBridge = await deploy(contractName, {
+        from: deployer,
+        args: [deployedContracts.wXTM, deployer],
+        deterministicDeployment: salt,
+        log: true,
+        waitConfirmations: 1,
+        skipIfAlreadyDeployed: false,
+    })
+
+    console.log(`wXTMBridge contract deployed to network: ${hre.network.name}, address: ${wXTMBridge.address}`)
+
+    await verify(wXTMBridge.address, [deployedContracts.wXTM, deployer])
+}
+
+deploy.tags = [contractName]
+
+export default deploy
