9b0b284d5ad7fe42ef52b1daa6f275dfd5487386
Nlferu <wargacki.patryq@gmail.com>
2025-05-12 12:09:13 +0200

    Audit wXTM-005: Unused contract implementation -> fix

--- contracts/wXTMBridge.sol
+++ contracts/wXTMBridge.sol
@@ -1,20 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import { IwXTM } from "./interfaces/IwXTM.sol";
 
-contract wXTMBridge is Ownable {
+contract wXTMBridge {
     using SafeERC20 for IERC20;
 
     address private immutable wXTM;
 
     event TokensUnwrapped(address indexed from, string targetTariAddress, uint256 indexed amount);
 
-    constructor(address _wXTM, address _delegate) Ownable(_delegate) {
+    constructor(address _wXTM) {
         wXTM = _wXTM;
     }
 
